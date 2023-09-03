2f24aef21f942ebd830305f0786f2e1207e30875
Mauro-Molina <tutowebgoo@gmail.com>
2023-09-02 21:57:59 -0400

    Añadir hoja de estilos en function php

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+
+//Add main menu
+
 function gymfitnees_menus(){
     register_nav_menus(array(
         'menu-principal' => __( 'Menu Principal' )
@@ -7,3 +10,12 @@ function gymfitnees_menus(){
 }
 
 add_action('init', 'gymfitnees_menus');
+
+//Style and Scripts
+
+function gymfitnees_scripts_styles(){
+    //style.css
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+}
+
+add_action('wp_enqueue_scripts', 'gymfitnees_scripts_styles');
