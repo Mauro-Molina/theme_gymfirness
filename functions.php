685820f481d01e96f8181091d2f62eab02891f93
Mauro-Molina <tutowebgoo@gmail.com>
2023-09-03 20:35:08 -0400

    Añadir Normalize y Gogle Fonts

--- functions.php
+++ functions.php
@@ -14,8 +14,13 @@ add_action('init', 'gymfitnees_menus');
 //Style and Scripts
 
 function gymfitnees_scripts_styles(){
+    $path = get_template_directory_uri();
+    //normalize
+    wp_enqueue_style('normalize', $path . '/assents/css/normalize.css', array(), '8.0.1');
     //style.css
     wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
+    //gogle fonts
+    wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Raleway:wght@500&family=Staatliches&display=swap', array(), '1.0.0');
 }
 
 add_action('wp_enqueue_scripts', 'gymfitnees_scripts_styles');
