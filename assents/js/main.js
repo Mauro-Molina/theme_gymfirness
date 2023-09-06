eb589ae13f3b664258a787418428a39260617452
Mauro-Molina <tutowebgoo@gmail.com>
2023-09-05 21:30:34 -0400

    Terminacion del menu de navegacion

---
+++ assents/js/main.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($){
+    $('.menu-principal .menu').slicknav({
+        label: '',
+        appendTo: '.site-header'
+    });
+ });
+ 
--- functions.php
+++ functions.php
@@ -21,6 +21,19 @@ function gymfitnees_scripts_styles(){
     wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
     //gogle fonts
     wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Raleway:wght@500&family=Staatliches&display=swap', array(), '1.0.0');
+
+    //Slick Nav css
+    wp_enqueue_style('SlickNav', $path . '/assents/css/slicknav.min.css', array(), '1.0.10');
+
+    //Slick Nav Js
+    wp_enqueue_script('slicknavJS', $path . '/assents/js/slicknav.min.js', array('jquery'), '1.0.10', true);
+
+    //Main JS 
+
+    wp_enqueue_script('mainJS', $path . '/assents/js/main.js', array('jquery', 'slicknavJS'), '1.0.0', true);
+
+    //Jquery Library
+    wp_enqueue_script('JQuery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'gymfitnees_scripts_styles');
