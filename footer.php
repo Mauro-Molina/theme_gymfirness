c81df036e55ddb921508bcaabec35c3880564620
Mauro-Molina <tutowebgoo@gmail.com>
2023-09-08 19:57:41 -0400

    Creando y poniendo estilos a la seccion de footer

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
 
 
-<footer class="site-footer">
-    <p>site-footer</p>
+<footer class="site-footer contenedor">
+    <hr>
+
+    <div class="contenido-footer">
+        <!--Navegacion -->
+        <?php 
+            $args = array(
+                'theme_location' => 'menu-principal',
+                'container' => 'nav',
+                'container_class' => 'menu-principal'
+                );
+                    wp_nav_menu($args);
+        ?>
+        <!--Navegacion -->
+        <p class="copy">Todos los derechos reservados <?php get_bloginfo('name') . " " . date('Y'); ?></p>
+    </div>
 </footer>
 
 
