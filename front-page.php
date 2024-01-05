397a3f50f8f567c593082a9b50a4866839498c4c
Mauro-Molina <tutowebgoo@gmail.com>
2024-01-04 21:55:12 -0500

    Home Page

---
+++ front-page.php
@@ -0,0 +1,151 @@
+<?php  get_header('front');?>
+
+
+<section class="bienvenida text-center seccion">
+    <h2 class="texto-primario"><?php the_field('encabezado'); ?></h2>
+    <p><?php the_field('contenido'); ?></p>
+</section>
+
+<div class="seccion-area">
+    <ul class="contenedor-area">
+        <!-- Area 1 -->
+        <li class="area">
+            <?php 
+                $area1 = get_field('area1');
+                $imagen = wp_get_attachment_image_src($area1['area_imagen1'], 'mediano')[0];
+            ?>
+
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html( $area1['area_texto'] ); ?></p>
+        </li>
+        <!-- Area 1 -->
+
+        <!-- Area 2 -->
+        <li class="area">
+            <?php 
+                $area2 = get_field('area2');
+                $imagen = wp_get_attachment_image_src($area2['area_imagen'], 'mediano')[0];
+            ?>
+
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html( $area2['area_texto'] ); ?></p>
+        </li>
+        <!-- Area 2 -->
+
+        <!-- Area 3 -->
+        <li class="area">
+            <?php 
+                $area3 = get_field('area3');
+                $imagen = wp_get_attachment_image_src($area3['area_imagen'], 'mediano')[0];
+            ?>
+
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html( $area3['area_texto'] ); ?></p>
+        </li>
+        <!-- Area 3 -->
+
+        <!-- Area 4 -->
+        <li class="area">
+            <?php 
+                $area4 = get_field('area4');
+                $imagen = wp_get_attachment_image_src($area4['area_imagen'], 'mediano')[0];
+            ?>
+
+            <img src="<?php echo esc_attr($imagen); ?>" alt="">
+            <p><?php echo esc_html( $area4['area_texto'] ); ?></p>
+        </li>
+        <!-- Area 4 -->
+    </ul>
+</div>
+
+<section class="clases">
+    <div class="contenedor seccion">
+        <h2 class="text-center texto-primario">Nuestras Clases</h2>
+        <?php gymfitnees_lista_clases(4); ?>
+        
+        <div class="contenedor-boton">
+            <a href="<?php echo esc_url (  get_permalink( get_page_by_title('Nuestras Clases') ) ); ?>" class="boton boton-primario">Ver todas las clases</a>
+        </div>
+    </div>
+</section>
+
+<section class="instructores">
+    <div class="contenedor seccion">
+        <h2 class="text-center texto-primario">Nuestros Instructores</h2>
+        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure libero perferendis voluptate nihil incidunt perspiciatis impedit, dolorem architecto ipsa quos porro explicabo, aliquid omnis quasi adipisci! Quibusdam temporibus fuga atque!</p>
+    
+        <ul class="listado-instructores">
+            <?php
+                $args = array(
+                    'post_type' => 'instructor',
+                    'posts_per_page' => 30
+                );
+
+                $instructores = new WP_Query($args);
+                while ($instructores->have_posts()) : $instructores->the_post();
+            ?>
+
+            <li class="instructores">
+                <?php the_post_thumbnail('mediano'); ?>
+                <div class="contenido text-center">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
+
+                    <div class="especialidad">
+                        <?php
+                            $esp = get_field('especialidad');
+
+                            foreach ($esp as $e): ?>
+
+                                <span class="etiqueta"><?echo $e;?></span>
+                            <?php endforeach;
+                        ?>
+                    </div>
+                </div>
+            </li>
+
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+    </div>
+</section>
+
+<section class="testimoniales">
+    <h2 class="text-center text0-blanco">Testimoniales</h2>
+
+    <dic class="contenedor-testimoniales">
+        <ul class="listado-testimoniales">
+            <?php
+                $args = array(
+                    'post_type' => 'testimonial',
+                    'post_per_page' => 10,
+                );
+
+                $testimoniales = new WP_Query($args);
+                while ($testimoniales->have_posts(  ) ) : $testimoniales->the_post(); ?>
+
+                <li class="testimonial">
+                    <blockquote>
+                        <?php the_content(); ?>
+                    </blockquote>
+
+                    <footer class="testimonial-footer">
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                    </footer>
+
+                    <?php the_title(); ?>
+                </li>
+
+                <?php endwhile; wp_reset_postdata(); ?>
+         
+        </ul>
+    </dic>
+</section>
+
+<section class="blog contenedor seccion">
+    <h2 class="text-center text-primario">Nuestro Blog</h2>
+    <p class="text-center">Aprende de nuestros instructores</p>
+
+    
+</section>
+
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -29,6 +29,15 @@ function gymfitnees_scripts_styles(){
     //LigthBox Css
     wp_enqueue_style( 'ligthbox', $path . '/assents/css/lightbox.min.css', array(), '1.0.0' );
     }
+    
+    if(is_page('inicio')) { 
+        //BXSLider CSS
+        wp_enqueue_style( 'bxslider css', $path . '/assents/css/bxslider.css', array(), '4.2.12' );
+        }
+   
+    //leaflet js
+    wp_enqueue_style( 'leaflet', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', array(), '1.9.4' );
+    
 
     //Slick Nav Js
     wp_enqueue_script('slicknavJS', $path . '/assents/js/slicknav.min.js', array('jquery'), '1.0.10', true);
@@ -41,7 +50,16 @@ function gymfitnees_scripts_styles(){
     wp_enqueue_script('ligthbox js', $path . '/assents/js/lightbox.min.js', array('jquery'), '1.0.0', true);
     }
 
-    //Jquery Library
+    if(is_page('inicio')) { 
+        //bxslider js
+        wp_enqueue_script('bxslider js', $path . '/assents/js/bxslider.js', array('jquery'), '4.2.12', true);
+        }
+
+    //leaflet js
+    wp_enqueue_script('leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js', array('jquery'), '1.9.4', true);
+    
+
+    //Jquery Library 
     wp_enqueue_script('JQuery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
 }
 
@@ -71,6 +89,7 @@ add_action( 'after_setup_theme', 'gymfitnees_setup');
 /* Consultas perzonalizables */
 
   require get_template_directory() . '/inc/queris.php';
+  require get_template_directory() . '/inc/shortcode.php';
 
   /* definir los widgets */
 
@@ -95,3 +114,33 @@ add_action( 'after_setup_theme', 'gymfitnees_setup');
   }
 
   add_action( 'widgets_init', 'gymfitnees_widgets' );
+
+
+//image Hero
+
+function gymfitnees_hero_image(){
+    //obtener el id de la pagina principal
+    $front_page_id = get_option('page_on_front');
+
+    //id de la imagen
+    $id_imagen = get_field('image', $front_page_id);
+
+    //Obtenner la imagen
+    $imagen = wp_get_attachment_image_src($id_imagen, 'full')[0];
+
+    //Style css
+    wp_register_style('custom', false);
+    wp_enqueue_style( 'custom' );
+
+
+    //imagen destacada
+    $imagen_destacada_css = "
+        body.home .site-header {
+            background-image: linear-gradient( rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url($imagen);
+        }
+    ";
+
+    wp_add_inline_style('custom', $imagen_destacada_css);
+}
+
+add_action( 'init', 'gymfitnees_hero_image' );
